5c74ba0c6f89588e2814448c91415b372a42f139
Mateus Gomes <mateuscostagomes000@gmail.com>
2025-04-24 21:38:47 -0300

    feat: add homepage struct

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,42 @@ import './styles/theme.css'
 import './styles/global.css'
 
 import { Container } from './components/Container'
-import { Heading } from './components/Heading'
+import { Logo } from './components/Logo'
+import { Menu } from './components/Menu'
+import { CountDown } from './components/CountDown'
 
 
 export function App(){
     return (
         <>
             <Container>
-                    <Heading>LOGO</Heading>
+                <Logo/>
             </Container>
             <Container>
-                    <Heading>Menu</Heading>
+                <Menu/>
+            </Container>
+            <Container>
+                <CountDown/>
+            </Container>
+            <Container>
+                <form className='form' action="">
+                    <div className="formRow">
+                        <label htmlFor="input">Task</label>
+                        <input type="text" id='input' />
+                    </div>
+                    <div className="formRow">
+                        <p>
+                            Lorem ipsum dolor sit amet.
+                        </p>
+                    </div>
+                    <div className="formRow">
+                        <p>Ciclos</p>
+                        <p>0 0 0 0 0 0</p>
+                    </div>
+                    <div className="formRow">
+                        <button>Enviar</button>
+                    </div>
+                </form>
             </Container>
         </>
     )
