ccfaaab837b2bf1d66cd8ef1933fad7b819a1839
Mateus Gomes <mateuscostagomes000@gmail.com>
2025-04-24 22:48:41 -0300

    feat: add Cycles and DefaultButton components.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import { Logo } from './components/Logo'
 import { Menu } from './components/Menu'
 import { CountDown } from './components/CountDown'
 import { DefaultInput } from './components/DefaultInput'
+import { Cycles } from './components/Cycles'
+import { DefaultButton } from './components/DefaultButton'
+import { PlayCircleIcon, StopCircleIcon } from 'lucide-react'
 
 
 export function App(){
@@ -31,11 +34,10 @@ export function App(){
                         </p>
                     </div>
                     <div className="formRow">
-                        <p>Ciclos</p>
-                        <p>0 0 0 0 0 0</p>
+                        <Cycles/>
                     </div>
                     <div className="formRow">
-                        <button>Enviar</button>
+                        <DefaultButton icon={<StopCircleIcon/>} color='red' />
                     </div>
                 </form>
             </Container>
--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -5,11 +5,11 @@ type DefaultInputProps = {
     label: string
 } & React.ComponentProps<'input'>
 
-export function DefaultInput({ type, id, label, ...rest }: DefaultInputProps){
+export function DefaultInput({ type, id, label, ...props }: DefaultInputProps){
     return(
         <>
             {label && <label htmlFor={id}>{label}</label>}
-            <input className={styles.input} type={type} id={id} {...rest} />
+            <input className={styles.input} type={type} id={id} {...props} />
         </>
     )
 }
