e1077d3696e96af1ba7def6d09256d873884ce84
Mateus Gomes <mateuscostagomes000@gmail.com>
2025-04-28 22:39:52 -0300

    feat: add function submit main form button

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,14 +1,64 @@
 import { StopCircleIcon } from "lucide-react";
+import { useRef } from "react";
 
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 
+import { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
+import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
+
 export function MainForm(){
+    const { state, setState } = useTaskContext()
+    const TaskNameInput = useRef<HTMLInputElement>(null)
+
+    // cycles
+    const nextCycle = getNextCycle(state.currentCycle)
+    const nextCycleType = getNextCycleType(nextCycle)
+
+    const handleCreateNewTask = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        
+        if(!TaskNameInput.current) return
+
+        const taskName = TaskNameInput.current.value.trim()
+
+        if(!taskName){
+            alert('Digite o nome da tarefa')
+            return
+        }
+
+        const newTask: TaskModel = {
+            id: Date.now().toString(),
+            name: taskName,
+            startDate: Date.now(),
+            completeDate: null,
+            interruptDate: null,
+            duration: state.config[nextCycleType],
+            type: nextCycleType
+        }
+
+        const secondsRemaining = newTask.duration * 60
+
+        setState(prevState => {
+            return {
+                ...prevState,
+                activeTask: newTask,
+                currentCycle: nextCycle,
+                secondsRemaining,
+                formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
+                tasks: [...prevState.tasks, newTask]
+            }
+        })
+    }
+
     return (
-        <form className='form' action="">
+        <form onSubmit={handleCreateNewTask} className='form' action="">
             <div className="formRow">
-                <DefaultInput type='text' id='defaultInput' label='Task' placeholder='Digite algo' />
+                <DefaultInput type='text' id='defaultInput' label='Task' placeholder='Digite algo' ref={TaskNameInput} />
             </div>
             <div className="formRow">
                 <p>
--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { initialTaskState } from "./initialTaskState"
 import { TaskContext } from "./TaskContext"
 
@@ -8,6 +8,11 @@ interface TaskContextProviderProps{
 
 export function TaskContextProvider({ children }: TaskContextProviderProps){
     const [state, setState] = useState(initialTaskState)
+
+    // Monitoring state
+    useEffect(() => {
+        console.log(state)
+    }, [state])
 
     return(
         <TaskContext.Provider value={{ state, setState }}>
